7e93ba2b751443bccdef2513aa382f378354f347
beka3301 <bekturhanazarov98@gmail.com>
2022-12-13 19:35:03 +0600

    d

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
 
+        data();
+    }
+    public static void data(){
+
+        System.out.println("passport issued: "+"serial: "+"number: ");
     }
 }
